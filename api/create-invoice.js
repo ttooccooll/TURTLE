3945f53fe1e063a131870d8e3f18232d6ffd3d9e
Jason <jasonbohio@gmail.com>
2025-12-12 11:34:31 -0500

    hm

--- api/create-invoice.js
+++ api/create-invoice.js
@@ -56,7 +56,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       paymentRequest: invoice.paymentRequest,
-      externalId: invoiceId
+      externalId: invoice.externalId
     });
 
   } catch (err) {
